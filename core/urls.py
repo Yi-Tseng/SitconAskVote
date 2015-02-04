46484d20f2bca963357242fbe94a4bdecccb5e6f
Takeshi <a86487817@gmail.com>
2015-02-04 12:09:59 +0800

    add question url setting

--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,6 @@
 from django.conf.urls import patterns, include, url
-from django.contrib import admin
+import question
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'SitconQuestionCorner.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^question/', include(question.urls)),
 )
