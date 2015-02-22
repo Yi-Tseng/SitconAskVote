0e48ef0adaf2dfa66248b846dbb37ec68238a047
Takeshi <a86487817@gmail.com>
2015-02-22 22:49:21 +0800

    add RWD and finish view design

---
+++ Prototype/SitconQueCorner/javascripts/scripts.js
@@ -0,0 +1,43 @@
+$(".nav-bar-btn").click(function(e) {
+    var style = $(".nav-bar").attr("style");
+
+    if(style === "display: block;") {
+        $(".nav-bar").attr("style", "")
+    } else {
+        $(".nav-bar").attr("style", "display: block;")
+    }
+});
+
+
+$(".btn-want-this-question").click(function(e) {
+    console.log(e.target.id);
+    var sel = "#" + e.target.id;
+
+    if($(sel).hasClass("wanted")) {
+        $(sel).removeClass("wanted");
+        $(sel).html("想聽");
+        // TODO: change database
+    } else {
+        $(sel).addClass("wanted");
+        $(sel).html("已列入想聽名單");
+        // TODO: change database
+    }
+});
+
+
+$(".btn-collapse").click(function(e) {
+
+    if(e.target.innerHTML === "▼") {
+        var href = e.target.href;
+        var target = href.split("#")[1];
+        $("#" + target).hide();
+        e.target.innerHTML = "►";
+    } else {
+        var href = e.target.href;
+        var target = href.split("#")[1];
+        $("#" + target).show();
+        e.target.innerHTML = "▼";
+    }
+
+
+});
