2657292212365145ec9d3d501db0de24f4872343
Takeshi <a86487817@gmail.com>
2015-02-22 17:56:32 +0000

    fix url and bugs

--- users/views/users.py
+++ users/views/users.py
@@ -2,7 +2,8 @@
 
 from django.contrib.auth.models import User, Group
 from django.shortcuts import render
+from django.views.decorators.debug import sensitive_post_parameters
 
-@sensitive_variables('password')
+@sensitive_post_parameters('password')
 def register(request):
     username = request.POST.get('username')
