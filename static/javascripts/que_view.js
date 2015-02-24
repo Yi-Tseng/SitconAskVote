69777da2b3d75a8e738b5a3e5107ed90804cd4b0
Takeshi <a86487817@gmail.com>
2015-02-24 16:36:57 +0800

    add refresh of question view

--- static/javascripts/que_view.js
+++ static/javascripts/que_view.js
@@ -3,5 +3,9 @@
 setInterval(function() {
 	$.get('/question/want', function(data) {
 		console.log(data);
+		for(k in data) {
+			$("#pop-" + k + " .title b").html("(" + data[k] + "人想聽)")
+		}
+		
 	}, 'json')
 }, 5000);
