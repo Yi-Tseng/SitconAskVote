70d5e41d8fbb33dbc726b10937281255aaf58fa6
Takeshi <a86487817@gmail.com>
2015-02-26 22:53:08 +0800

    finish fb login and add email scope

--- facebook/views.py
+++ facebook/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import redirect
+from django.shortcuts import redirect, render
+from django.contrib.auth.models import User
+import django.contrib.auth as auth
 from facepy import GraphAPI
-import json
+from os import urandom
+from base64 import urlsafe_b64encode
 
 # Create your views here.
 def login(request):
@@ -13,6 +16,29 @@ def login(request):
     access_token = request.GET['token']
     graph = GraphAPI(access_token)
 
-    print graph.get('me')
+    info = graph.get('me?fields=name,email,verified')
+    name = info['name']
+    verified = info['verified']
+    email = info['email']
+
+    if not verified:
+        return render(request, 'login.html', {'error': 'FB 帳號未驗證'})
+
+    try:
+        user = User.objects.get(email=email)
+        auth.login(request, user)
+
+    except User.DoesNotExist:
+        user = User()
+        user.username = email
+        user.email = email
+        user.last_name = name
+        password = urlsafe_b64encode(urandom(32))[:-2]
+        user.set_password(password)
+        user.is_active = True
+        user.save()
+        auth.login(request, user)
 
     return redirect('/')
+
+
