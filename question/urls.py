c3a96967fcaa15fd3711288707c72c1ab1bb5c8f
Takeshi <a86487817@gmail.com>
2015-03-01 11:11:53 +0000

    fix question url

--- question/urls.py
+++ question/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import patterns, url
 from question import views
 urlpatterns = patterns('',
-	url(r'^ask$', views.view_question),
+    url(r'^$', views.view_question),
     url(r'^ask$', views.ask),
     url(r'^view$', views.view_question),
     url(r'^want$', views.want_listen),
